40394a83830004a97c2874cbdda80f6afb70cc7e
Andreza Cri <andrezadesenvolvedoraweb@gmail.com>
2022-08-05 12:36:44 -0300

    função menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function exibirMenu () {
+    document.getElementById("header").style.display = "block";
+}
+
+function fecharMenu () {
+    document.getElementById("header").style.display = "none";
+}
