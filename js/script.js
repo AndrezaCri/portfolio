f4d2d7190dc4f62dbae7fd65b4ae6c86c0f91da5
Andreza Cri <andrezadesenvolvedoraweb@gmail.com>
2022-08-23 17:46:22 -0300

    últimas confirmações no projeto

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ function exibirMenu () {
 function fecharMenu () {
     document.getElementById("header").style.display = "none";
 }
+
+function ancora(anchor){
+    window.location.href = "#"+anchor;
+    fecharMenu();
+}
+
